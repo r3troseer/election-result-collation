c5d98b293f06dac76d6bcc6004b63b1d0158fa16
r3troseer <piagbo3d@gmail.com>
2023-03-31 03:18:16 +0100

    added homepage for easy navigation

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/",views.home, name='home'),
+    path("",views.home, name='home'),
     path("poll_result/", views.polling_unit_result, name="poll_result"),
     path("lga_results/", views.lga_results, name="lga_results"),
     path('add_polling_unit_result/', views.add_polling_unit_result, name='add_polling_unit_result'),
